2ddf049cf84ea53b0cecf6e5a5a982fac58d9142
Daryl <darylkue@gmail.com>
2022-04-02 08:39:50 +0200

    first commit for the ToDoList_v1 project

---
+++ app.js
@@ -0,0 +1,43 @@
+//packages
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+//set up listen port
+app.listen(3000, function(){
+    console.log("server is running on port 3000...")
+});
+
+let items = ["Power on computer","Code JavaScript","Play video games"];
+
+//get methods
+app.get("/", function(req, res){
+    let today = new Date();
+    let options = {
+        day : "numeric",
+        weekday : "long",
+        year : "numeric"
+    };
+
+    let day = today.toLocaleDateString("en-US",options);
+    res.render("index", {kindOfDay: day, newItem: items});
+});
+
+app.post("/", function(req, res){
+    let item = req.body.newItem;
+    if (item === "") {
+        res.send("No item was specified");
+    }
+    else{
+        items.push(item);
+        res.redirect("/");
+    }
+});
+
+app.post("/noItem", function(req, res){
+    res.redirect("/");
+});
